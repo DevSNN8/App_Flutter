cfdad760fe00b8283c177706ea4eba8858bf3617
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-04-29 13:27:27 +0700

    listt

--- lib/main.dart
+++ lib/main.dart
@@ -9,15 +9,47 @@ class myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "mark",
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text("Beckham"),
+    return MaterialApp(title: "mark", home: myhomepage());
+    ;
+  }
+}
+
+class myhomepage extends StatefulWidget {
+  const myhomepage({super.key});
+
+  @override
+  State<myhomepage> createState() => _myhomepageState();
+}
+
+class _myhomepageState extends State<myhomepage> {
+  int number = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    List<Widget> data = [];
+    for (var i = 0; i < 10; i++) {
+      data.add(Text("Press Add $i"));
+    }
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Prgram"),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: data,
         ),
-        body: Text("helloooooo"),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: addnumber,
+        child: Icon(Icons.add),
       ),
     );
-    ;
+  }
+
+  void addnumber() {
+    setState(() {
+      number++;
+    });
   }
 }
