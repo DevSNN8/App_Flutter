30b6d054d703e7db1c972091f54844c27a032628
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-05-12 11:26:06 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,42 +1,92 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(Myapp());
+  runApp(MyApp());
 }
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Game shop",
-      home: Myhome(),
+      home: Scaffold(
+        body: MyShoppingPage(),
+      ),
     );
   }
 }
 
-class Myhome extends StatefulWidget {
-  const Myhome({super.key});
+var menuButton = Center(
+  child: Ink(
+    decoration: const ShapeDecoration(
+      color: Colors.grey,
+      shape: CircleBorder(),
+    ),
+    child: IconButton(
+      icon: Icon(Icons.menu_sharp),
+      color: Colors.black,
+      onPressed: () {},
+    ),
+  ),
+);
 
-  @override
-  State<Myhome> createState() => _MyhomeState();
-}
-
-class _MyhomeState extends State<Myhome> {
+class MyShoppingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Center(
-            child: Text(
-          "Press Button add?",
-          style: TextStyle(fontSize: 30, color: Colors.red),
-        )),
-        floatingActionButton: FloatingActionButton.extended(
-          backgroundColor: const Color.fromRGBO(82, 170, 94, 1.0),
-          onPressed: () {},
-          label: const Text('Edit', style: TextStyle(color: Colors.white)),
-          icon: const Icon(Icons.edit, color: Colors.white, size: 25),
-        ));
+    return Material(
+      child: Container(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
   }
 }
+
+var cartButton = Center(
+  child: Ink(
+    decoration: const ShapeDecoration(
+      color: Colors.grey,
+      shape: CircleBorder(),
+    ),
+    child: IconButton(
+      icon: Icon(IconData(59870, fontFamily: 'MaterialIcons')),
+      color: Colors.black,
+      onPressed: () {},
+    ),
+  ),
+);
+
+var menuBar = Row(
+  children: [
+    menuButton,
+    cartButton,
+  ],
+);
+
+var menuBaar = Row(
+  children: [
+    Expanded(
+        child: Row(children: [menuButton], textDirection: TextDirection.ltr)),
+    Expanded(
+        child: Row(children: [cartButton], textDirection: TextDirection.rtl)),
+  ],
+);
+
+var shoesName = Text(
+  "Space Shoes",
+  style: TextStyle(fontFamily: 'Sora', fontSize: 36),
+);
+
+var shoesImage = Container(
+  decoration: BoxDecoration(
+      color: const Color.fromARGB(255, 232, 237, 243),
+      image: const DecorationImage(
+        image: NetworkImage(
+            'https://images.pexels.com/photos/2529148/pexels-photo-2529148.jpeg?cs=srgb&dl=pexels-melvin-buezo-2529148.jpg&fm=jpg'),
+        fit: BoxFit.cover,
+      ),
+      border: Border.all(width: 1.0, color: Color.fromARGB(255, 232, 237, 243)),
+      borderRadius: BorderRadius.circular(10)),
+  width: 297,
+  height: 288,
+);
