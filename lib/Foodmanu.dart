7135da0cabbc9e79bb03a8d0e087322c2e9018ba
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-05-18 12:45:02 +0700

    login

--- lib/Foodmanu.dart
+++
@@ -1,7 +0,0 @@
-class foodmenu {
-  String name;
-  String price;
-  String img;
-
-  foodmenu(this.name, this.price, this.img);
-}
--- lib/main.dart
+++ lib/main.dart
@@ -1,57 +1,19 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-import 'package:http/http.dart' as http;
+import 'package:flutter_application_2/login_page.dart';
 
 void main() {
-  runApp(myapp());
+  runApp(Myapp());
 }
 
-class myapp extends StatelessWidget {
-  const myapp({super.key});
+class Myapp extends StatelessWidget {
+  const Myapp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(title: "mark", home: myhomepage());
-    ;
-  }
-}
-
-class myhomepage extends StatefulWidget {
-  const myhomepage({super.key});
-
-  @override
-  State<myhomepage> createState() => _myhomepageState();
-}
-
-class _myhomepageState extends State<myhomepage> {
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    getExchangeRate();
-  }
-
-  Future<void> getExchangeRate() async {
-    var url = "https://api.exchangeratesapi.io/latest?symbols=USD,THB";
-    var response = await http.get(url as Uri);
-    print(response.body);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            "MY account",
-            style: TextStyle(
-                fontSize: 25,
-                color: Color.fromARGB(255, 207, 54, 54),
-                fontWeight: FontWeight.bold),
-          ),
-        ),
-        body: Column(
-          children: [],
-        ));
+    return MaterialApp(
+      title: 'MarkVen Down',
+      theme: ThemeData.dark(),
+      home: LoginPage(),
+    );
   }
 }
