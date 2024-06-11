60a1eb439aa6bb18f7c472d16414bf75a4281ef9
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-06-11 10:09:50 +0700

    t

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/components/bottom_nav_bar.dart';
+
+import 'cart_page.dart';
+import 'shop_page.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,8 +12,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //this selected index is to control the bottom nav bar
+  int _selectedInex = 0;
+
+  void navigateBottomBar(int index) {
+    setState(() {
+      _selectedInex = index;
+    });
+  }
+
+  final List<Widget> _pages = [
+    const ShopPage(),
+    const CartPage(),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      bottomNavigationBar: MyBottomNavBar(
+        onTabChange: (index) => navigateBottomBar(index),
+      ),
+      body: _pages[_selectedInex],
+      backgroundColor: Colors.grey,
+    );
   }
 }
--- lib/components/bottom_nav_bar.dart
+++ lib/components/bottom_nav_bar.dart
@@ -1,11 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:google_nav_bar/google_nav_bar.dart';
 
-class MyBottomNavBAr extends StatelessWidget {
-  const MyBottomNavBAr({super.key});
+class MyBottomNavBar extends StatelessWidget {
+  void Function(int)? onTabChange;
+  MyBottomNavBar({super.key, required this.onTabChange});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: GNav(tabs:);
+      padding: EdgeInsets.symmetric(vertical: 20),
+      child: GNav(
+        color: Colors.grey,
+        activeColor: Colors.grey[400],
+        tabActiveBorder: Border.all(color: Colors.white),
+        tabBackgroundColor: Colors.grey.shade300,
+        mainAxisAlignment: MainAxisAlignment.center,
+        tabBorderRadius: 16,
+        onTabChange: (value) => onTabChange!(value),
+        tabs: [
+          GButton(
+            icon: Icons.home,
+            text: 'Shop',
+          ),
+          GButton(
+            icon: Icons.shopping_bag_rounded,
+            text: 'Cart',
+          )
+        ],
+      ),
+    );
   }
 }
