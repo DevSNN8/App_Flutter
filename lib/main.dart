f74af94402e292b1668ecaed16a7c56cdaaaf28b
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-06-17 10:35:37 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,60 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
-void main() {
-  runApp(Myapp());
-}
+/// Flutter code sample for [Drawer].
 
-class Myapp extends StatelessWidget {
-  const Myapp({super.key});
+void main() => runApp(const DrawerApp());
+
+class DrawerApp extends StatelessWidget {
+  const DrawerApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Snaxk'),
+    return MaterialApp(
+      theme: ThemeData(useMaterial3: true),
+      home: Myapp(),
+    );
+  }
+}
+
+class Myapp extends StatefulWidget {
+  const Myapp({super.key});
+
+  @override
+  State<Myapp> createState() => _MyappState();
+}
+
+class _MyappState extends State<Myapp> {
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(
+      initialIndex: 1,
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Tabar'),
+          bottom: TabBar(tabs: <Widget>[
+            Tab(
+              icon: Icon(Icons.car_crash),
+            ),
+            Tab(
+              icon: Icon(Icons.add_a_photo),
+            ),
+            Tab(
+              icon: Icon(Icons.cloud_upload),
+            )
+          ]),
+        ),
+        body: TabBarView(children: <Widget>[
+          Center(
+            child: Text('Location'),
+          ),
+          Center(
+            child: Text('Photo'),
+          ),
+          Center(
+            child: Text('Cloud'),
+          ),
+        ]),
       ),
     );
   }
