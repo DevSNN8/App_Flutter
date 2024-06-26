c9b9f3c12f4d20008ffa06cdc955f061c3949452
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-06-26 09:28:14 +0700

    j

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,14 @@
 import 'package:flutter/material.dart';
 
-/// Flutter code sample for [Drawer].
-
-void main() => runApp(const DrawerApp());
-
-class DrawerApp extends StatelessWidget {
-  const DrawerApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(useMaterial3: true),
-      home: Myapp(),
-    );
-  }
+void main() {
+  runApp(Myapp());
 }
 
-class Myapp extends StatefulWidget {
+class Myapp extends StatelessWidget {
   const Myapp({super.key});
 
   @override
-  State<Myapp> createState() => _MyappState();
-}
-
-class _MyappState extends State<Myapp> {
-  @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-      initialIndex: 1,
-      length: 3,
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Tabar'),
-          bottom: TabBar(tabs: <Widget>[
-            Tab(
-              icon: Icon(Icons.car_crash),
-            ),
-            Tab(
-              icon: Icon(Icons.add_a_photo),
-            ),
-            Tab(
-              icon: Icon(Icons.cloud_upload),
-            )
-          ]),
-        ),
-        body: TabBarView(children: <Widget>[
-          Center(
-            child: Text('Location'),
-          ),
-          Center(
-            child: Text('Photo'),
-          ),
-          Center(
-            child: Text('Cloud'),
-          ),
-        ]),
-      ),
-    );
+    return MaterialApp();
   }
 }
