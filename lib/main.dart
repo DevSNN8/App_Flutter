0ad57638be7bf886a978c22699794f201b3b13dd
DevSNN8 <85948511+DevSNN8@users.noreply.github.com>
2024-07-01 11:15:16 +0700

    l

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,39 @@ class Myapp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Toy fo man')),
+        body: Center(
+          child: smack(),
+        ),
+      ),
+    );
+  }
+}
+
+class smack extends StatefulWidget {
+  const smack({super.key});
+
+  @override
+  State<smack> createState() => _smackState();
+}
+
+class _smackState extends State<smack> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Column(
+          children: [
+            ElevatedButton(
+                onPressed: () {
+                  print('Mom With friend');
+                },
+                child: Text('Pussy'))
+          ],
+        ),
+      ),
+    );
   }
 }
